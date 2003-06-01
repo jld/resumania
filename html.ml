4d3d192a0eabf6e77a2f33ab93fc043513406912
Jed Davis <jldavis@cs.oberlin.edu>
2003-06-01 23:15:25 +0000

    more HTML nudging

--- html.ml
+++ html.ml
@@ -15,7 +15,9 @@ let preamble = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"
     H1 { font-size: 2.5em } H2 { font-size: 2em } H3 { font-size: 1.7em }\n\
     H4 { font-size: 1.4em } H5 { font-size: 1.2em } H6 { font-size: 1.0em }\n\
     .s1 { margin: .78em 0 0 } .s2 { margin: .55em 0 0 } .s3 { margin: .33em 0 0 }\n\
-    P { margin: .1667em 0 0 } UL { margin-top: .33em; margin-bottom: 0 } \n\
+    UL { margin-top: .3333em; margin-bottom: 0 } \
+    LI { margin-top: .1667em; margin-bottom: 0 } \
+    P { margin: .1667em 0 0 }\n\
     sub.tex { font-size: 1em; margin-left: -.1667em; \
               margin-right: -.125em; vertical-align: -.5ex }\n\
     sup.tex { font-size: .8em; margin-left: -.36em; \
@@ -50,7 +52,7 @@ let rec trans_blki = function
       let hno = if n<0 then 6 else if n>5 then 1 else 6-n
       in Printf.sprintf "<H%d align='center' class='head'>%s</H%d>"
 	hno (trans_ils il) hno
-  | Itemize(bll) -> entag "UL" 
+  | Itemize(bll) -> entag ~attr:"compact" "UL" 
 	(catmap (function bl -> entag "LI" (trans_bls bl)) bll)
 and trans_blk = function x -> (trans_blki x)^"\n"
 and trans_bls = function x -> catmap trans_blk x (*sigh*)
