2338dce058297d746ece60258b0ae2dba68b5236
Jed Davis <jld@panix.com>
2012-09-26 23:49:03 -0700

    Publish script, attempt 1

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e -u
+SRC=r2012
+DST=jld
+set -x
+git branch -D redacted
+git branch redacted master
+git filter-branch --force --tree-filter "$(pwd)/redact-tree.sh" redacted
+git checkout redacted
+./go "$SRC"
+git add --force "$DST".*
+git commit -m "Commit build products for publication"
+git checkout master # FIXME
